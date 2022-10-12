8f7144ddcec80cb0a0e1c92e799144faa3f9fe2d
hasan <34905@ma-web.nl>
2022-10-12 12:14:27 +0200

    haiku hasan

--- haiku.py
+++ haiku.py
@@ -6,11 +6,15 @@
 def HaikuX():
     print("haiku by:","Sean")
     print("Bleak break of the day")
-    print("A smallest, neat pebble calls")
+    print("A smallest, neat pebble calls")          
     print("in spite of the stone")
 
-
-
+def Haiku1():
+    print("haiku by:","-hasan-")
+    print("Arid abandon")
+    print("A pale, thundery ghost looks")
+    print("because of the king")
+    
 
 #laat de code hieronder met rust
 Haiku1()
